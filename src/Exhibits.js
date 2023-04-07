a5387f20e3827debdcea44e205f0c13c28ea960a
Erik Russell <erikrussells@gmail.com>
2023-04-06 22:51:12 -0400

    working on exhibits

--- src/Exhibits.js
+++ src/Exhibits.js
@@ -35,7 +35,7 @@ function Exhibits() {
               <div id="exhibit-img-div">
                 <img
                   id="exhibit-img"
-                  src="https://www.rainmakerart.co.uk/wp-content/uploads/2017/01/kachina-dolls-native-american-indian-artists.jpg"
+                  src="https://www.metmuseum.org/-/media/images/about-the-met/collection-areas/ancient-americas/department-page/ancientamericas_collection.jpg?sc_lang=en&h=720&w=1520&la=en&hash=0EC1EC268C181BFCD8FB4B0C92E6E345"
                   alt=""
                 />
               </div>
@@ -69,7 +69,7 @@ function Exhibits() {
               <div id="exhibit-img-div">
                 <img
                   id="exhibit-img"
-                  src="https://assets.sutori.com/user-uploads/image/3a65dc7a-19e1-4d9c-995a-8ee8e0af6ae8/320px-Al-Buraf_Hafifa.jpeg"
+                  src="https://cdn-aicbd.nitrocdn.com/YAftAITFjrnLdzaqWiKIELVxAeGHpmqa/assets/static/optimized/rev-92dc3be/wp-content/uploads/2020/02/amorsolo.jpg"
                   alt=""
                 />
                 <div id="exhibit-text-div">
@@ -89,8 +89,8 @@ function Exhibits() {
             <div className="exhibit">
               <div id="exhibit-coming-img-div">
                 <img
-                  id="exhibit-img"
-                  src="https://media.nga.gov/iiif/5dd2009f-7782-43d8-9892-be5733d8f43b/full/!740,560/0/default.jpg"
+                  id="coming-exhibit-img"
+                  src="https://static01.nyt.com/images/2022/04/07/arts/07winslowhomer-review-gulfstream/merlin_205011450_2c462920-3edd-4de5-8495-146dc4a1c437-mediumSquareAt3X.jpg"
                   alt=""
                 />
                 <div id="exhibit-text-div">
@@ -106,8 +106,8 @@ function Exhibits() {
             <div className="exhibit">
               <div id="exhibit-coming-img-div">
                 <img
-                  id="exhibit-img"
-                  src="https://www.rainmakerart.co.uk/wp-content/uploads/2017/01/kachina-dolls-native-american-indian-artists.jpg"
+                  id="coming-exhibit-img"
+                  src="http://media.britishmuseum.org/media/Repository/Documents/2014_9/30_8/e41842e5_b723_4a22_81f5_a3b600860b25/preview_00180352_001.jpg"
                   alt=""
                 />
               </div>
@@ -123,8 +123,8 @@ function Exhibits() {
             <div className="exhibit">
               <div id="exhibit-coming-img-div">
                 <img
-                  id="exhibit-img"
-                  src="https://nottinghammuseums.org.uk/wp-content/uploads/2021/02/the-vikings-and-anglo-saxons-1080x887-1.jpg"
+                  id="coming-exhibit-img"
+                  src="https://images.fineartamerica.com/images-medium-large-5/1-insects-jacob-hoefnagel-joris-hoefnagel-joris-hoefnagel-and-claes-jansz-visscher-ii.jpg"
                   alt=""
                 />
                 <div id="exhibit-text-div">
@@ -140,8 +140,8 @@ function Exhibits() {
             <div className="exhibit">
               <div id="exhibit-coming-img-div">
                 <img
-                  id="exhibit-img"
-                  src="https://assets.sutori.com/user-uploads/image/3a65dc7a-19e1-4d9c-995a-8ee8e0af6ae8/320px-Al-Buraf_Hafifa.jpeg"
+                  id="coming-exhibit-img"
+                  src="https://images.immediate.co.uk/production/volatile/sites/7/2018/01/Romulus-and-Remus-87ad9ea.jpg?quality=90&resize=800,534"
                   alt=""
                 />
                 <div id="exhibit-text-div">
@@ -157,8 +157,8 @@ function Exhibits() {
             <div className="exhibit">
               <div id="exhibit-coming-img-div">
                 <img
-                  id="exhibit-img"
-                  src="https://assets.sutori.com/user-uploads/image/3a65dc7a-19e1-4d9c-995a-8ee8e0af6ae8/320px-Al-Buraf_Hafifa.jpeg"
+                  id="coming-exhibit-img"
+                  src="https://www.brickunderground.com/sites/default/files/blog/images/iStock_86366777_MEDIUM.jpg"
                   alt=""
                 />
                 <div id="exhibit-text-div">
@@ -174,8 +174,8 @@ function Exhibits() {
             <div className="exhibit">
               <div id="exhibit-coming-img-div">
                 <img
-                  id="exhibit-img"
-                  src="https://assets.sutori.com/user-uploads/image/3a65dc7a-19e1-4d9c-995a-8ee8e0af6ae8/320px-Al-Buraf_Hafifa.jpeg"
+                  id="coming-exhibit-img"
+                  src="https://cdn.britannica.com/45/1245-004-676A6721/Kanagawa-Wave-woodblock-colour-print-series-part.jpg"
                   alt=""
                 />
                 <div id="exhibit-text-div">
